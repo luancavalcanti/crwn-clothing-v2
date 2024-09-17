9a2462f2875742c0c648ef9aacce275fe2e9ab46
luancavalcanti <luanscavalcanti@gmail.com>
2024-09-16 21:02:23 -0400

    added in our addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,14 @@ import { useContext } from 'react'
 import { CartContext } from '../../context/cart.context'
 
 export default function CartIcon(){
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
     function toggleIsCartOpen(){
         setIsCartOpen(!isCartOpen)
     }
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react'
 import Button from '../button/button.component'
 import './product-card.styles.scss'
+import { CartContext } from '../../context/cart.context'
 
 export default function ProductCard({ product }){
     const { name, price, imageUrl } = product
+    const { addItemToCart } = useContext(CartContext)
+
+    function addProductToCart(){
+        addItemToCart(product)
+    }
     return(
         <div className='product-card-container'>
             <img src={imageUrl} alt=''/>
@@ -10,7 +17,7 @@ export default function ProductCard({ product }){
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
         </div>
     )
 }
